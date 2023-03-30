2368ae04ed440673d1e9eea7011f9a4285acbf8c
s20501 <s20501@pjwstk.edu.pl>
2023-03-30 20:38:50 +0200

    Activity 1

--- zjazd_2/app/src/main/java/com/example/zjazd_2/Activity1.java
+++ zjazd_2/app/src/main/java/com/example/zjazd_2/Activity1.java
@@ -12,59 +12,49 @@ import android.widget.TextView;
 public class Activity1 extends AppCompatActivity {
 
 
-=    private double weight = 0.0;
+    private double weight = 0.0;
     private double height = 0.15;
-    //TextViews definitions
+
     private TextView heightTextView;
     private TextView weightTextView;
     private TextView totalTextView;
+
+    private EditText weightEditText;
+    private EditText heightEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
 
-        //looking up TextViews in layout
-        weightTextView = (TextView) findViewById(R.id.weightTextView);
-        heightTextView = (TextView) findViewById(R.id.heightTextView);
-        totalTextView = (TextView) findViewById(R.id.totalTextView);
-        totalTextView.setText(String.format(0 + ""));
-// setting TextWatchers for both weight and height fields
-        EditText weightEditText =
-                (EditText) findViewById(R.id.weightEditText);
+        weightTextView = findViewById(R.id.weightTextView);
+        heightTextView = findViewById(R.id.heightTextView);
+        totalTextView = findViewById(R.id.totalTextView);
+        totalTextView.setText("0");
+
+        weightEditText = findViewById(R.id.weightEditText);
         weightEditText.addTextChangedListener(weightEditTextWatcher);
-        EditText heightEditText =
-                (EditText) findViewById(R.id.heightEditText);
+
+        heightEditText = findViewById(R.id.heightEditText);
         heightEditText.addTextChangedListener(heightEditTextWatcher);
-
-
     }
 
-    //method calculate, calculating BMI, setting totalTextView as result
     private void calculate() {
-
-
         double total = weight / (height * height);
-
-
-        totalTextView.setText(String.format(total + ""));
+        totalTextView.setText(String.format("%.2f", total));
     }
 
-    // listeners object for TextViews weight and height text-changed events
     private final TextWatcher weightEditTextWatcher = new TextWatcher() {
-        // called when the user modifies weight or height field, or both
-        @Override
-        public void onTextChanged(CharSequence s, int start,
-                                  int before, int count) {
 
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
             try {
                 weight = Double.parseDouble(s.toString()) / 100.0;
-                weightTextView.setText(String.format(weight + ""));
+                weightTextView.setText(String.format("%.2f", weight));
             } catch (NumberFormatException e) {
                 weightTextView.setText("");
                 weight = 0.0;
             }
-//recalculate on change
             calculate();
         }
 
@@ -73,24 +63,21 @@ public class Activity1 extends AppCompatActivity {
         }
 
         @Override
-        public void beforeTextChanged(
-                CharSequence s, int start, int count, int after) {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         }
     };
+
     private final TextWatcher heightEditTextWatcher = new TextWatcher() {
 
         @Override
-        public void onTextChanged(CharSequence s, int start,
-                                  int before, int count) {
-
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
             try {
                 height = Double.parseDouble(s.toString()) / 100.0;
-                heightTextView.setText(String.format(height + ""));
+                heightTextView.setText(String.format("%.2f", height));
             } catch (NumberFormatException e) {
                 heightTextView.setText("");
                 height = 0.0;
             }
-//recalculate on change
             calculate();
         }
 
@@ -99,8 +86,8 @@ public class Activity1 extends AppCompatActivity {
         }
 
         @Override
-        public void beforeTextChanged(
-                CharSequence s, int start, int count, int after) {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         }
     };
+
 }
